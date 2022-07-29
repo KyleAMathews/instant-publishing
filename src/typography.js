cf561208109bc38606d5effbf4241b41bbf1223a
Kyle Mathews <mathews.kyle@gmail.com>
2022-07-28 19:53:08 -0700

    Reduce blockMarginBottom

--- src/typography.js
+++ src/typography.js
@@ -1,7 +1,7 @@
 import Typography from "typography"
 import theme from "typography-theme-fairy-gates"
 
-const typography = new Typography(theme)
+const typography = new Typography({ ...theme, blockMarginBottom: 0.5 })
 
 // Export helper functions
 export const { scale, rhythm, options } = typography
